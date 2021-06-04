5d7751e37333a5f16834a226796213be1c301b5a
KitanovskaMaryam <76153680+KitanovskaMaryam@users.noreply.github.com>
2021-06-04 08:42:05 +0200

    Update swiper.js

--- swiper.js
+++ swiper.js
@@ -1,10 +1,23 @@
 var swiper = new Swiper('.swiper-container', {
           slidesPerView: 2,
           centeredSlides: true,
-          spaceBetween: 40,
+          spaceBetween: 60,
           grabCursor: true,
+          speed: 200,
+
           pagination: {
             el: '.swiper-pagination',
             clickable: true,
           },
+          // od tuka e problemot, mi go aplicira nad 662 px a ne pod
+
+          breakpoints:{
+            662: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+              slidesPerGroup: 3,
+              loop: true,
+              loopFillGroupWithBlank: true,
+          }},
+
         });
